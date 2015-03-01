db658f2fae8042106a2a3b9bfc0f09f8194f62e0
jeffreyhorner <jeffrey.horner@gmail.com>
2015-03-01 02:36:43 -0600

    Benchmark #1

---
+++ BENCHMARK.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+# Modify the below paths to R to test, one on each line
+ALLR=(
+  ~/Projects/R/R-Array-Hash/bin/R
+  ~/Projects/R/optimized/devel/bin/R
+)
+DATASETS=(
+  SKEW.1mil
+  DISTINCT.500thou
+)
+RUNS=`seq 1 10`
+HASHSIZES=`Rscript -e 'cat(2^(10:19),"\n")'`
+
+OPTS='--vanilla --no-save'
+RESULTS='results.csv'
+
+rm -f $RESULTS error.log out.log
+
+for run in $RUNS; do
+for hashsize in $HASHSIZES; do
+for dataset in ${DATASETS[*]}; do
+for progname in ${ALLR[*]}; do
+    $progname $OPTS --args $dataset $hashsize $run $RESULTS < RUN.R \
+      1>>out.log 2>>error.log
+    echo '-----------------------' >> error.log
+done
+done
+done
+done
--- RUN.R
+++ RUN.R
@@ -1,30 +1,30 @@
+#
+#
+progname <- commandArgs()[1] # full path to R.
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args)!=4) stop("Need args: dataset hashsize run resultfile")
+
+datafile <- args[1]
+hashsize <- as.integer(args[2])
+run      <- as.integer(args[3])
+resfile  <- args[4]                  # will create if not exists
+
+if (!file.exists(datafile)) stop("No file!")
+
 # If cinline is installed, then we'll use basic replacements for file(), readLines()
 # and close() to eliminiate a little time to run the benchmark.
 source('fileio.R')
 
-# Size of largest significant cache: L1, L2, or L3? 
-CACHE_SIZE <- 6 * 2^10
+# For calculating memory cache misses
+library(pbdPAPI)
 
-# Create and write to an array the size of the cache
-flush_cache <- function(){
-    x <- numeric(CACHE_SIZE)
-    x[] <- rnorm(1)
-    invisible() 
-}
+set.seed(1)
 
-# Bogus value for each hash entry
-val <- 'token'
+# Test value for each hash entry
+val <- paste(sample(letters,size=7),collapse='')
 
-args <- commandArgs(trailingOnly=TRUE)
-
-if (length(args)!=4) stop("Need args: progname filename hashsize run")
-
-progname = args[1]
-FILENAME = args[2]
-hashsize = as.integer(args[3])
-run = as.integer(args[4])
-
-if (!file.exists(FILENAME)) stop("No file!")
 
 # The environment as hash to test
 e <- new.env(hash=TRUE, size=hashsize)
@@ -44,59 +44,68 @@ constructHash <- function(filename, hashsize=2^10){
     sum <- sum + (t2 - t1)
   }
   
-  invisible(list(elapsed=sum, env=e))
+  invisible(sum)
 }
 
 miss <- 0
 
 searchHash <- function(filename){
   con <- file(filename,open="r")
-  on.exit(close(con))
+  on.exit({close(con); lastline <<- line;})
   sum <- 0.0
   while(TRUE){
     line <- readLines(con,n=1)
     if (length(line) < 1) 
       break
 
-    # Miss should never miss!
     t1 <- proc.time()['elapsed']
-    if (get(line,envir=e)!='token') miss <<- miss + 1
+    m <- get(line,envir=e)
     t2 <- proc.time()['elapsed']
     sum <- sum + (t2 - t1)
+
+    # Miss should never miss!
+    if (m!=val) miss <<- miss + 1
   }
  
   invisible(sum);
 }
 
-invisible(gcinfo(verbose=TRUE)) # For reporting number of gc's
-flush_cache()
+# gcinfo(TRUE)
 invisible(gc(reset=TRUE)) # Reset is important here to estimate
-                          # the hash memory size.
-
-# If R allows it, turn off gc altogether
-if (any('on' %in% names(formals(gc))))
-  invisible(gc(on=FALSE))
+                          # the hash memory size on next gc().
 
 t1 <- proc.time()['elapsed']
-x <- constructHash(FILENAME,hashsize)
+x <- constructHash(datafile,hashsize)
 t2 <- proc.time()['elapsed']
 runsum <- t2 - t1;
 
-z <- gc() # Estimation of hash memory size
+z <- gc() # Estimation of hash memory size is z[11] + z[12]
 
-flush_cache()
 t1 <- proc.time()['elapsed']
-y <- searchHash(FILENAME)
+y <- searchHash(datafile)
 t2 <- proc.time()['elapsed']
 runsum <- runsum + (t2 - t1)
 
-write.table(
-  data.frame(
-    col1=rep(progname,8),
-    col2=c("construction","search","memory","runtime","hashsize","miss","filename","run"),
-    col3=c(x$elapsed, y, z[11] + z[12], runsum, hashsize, miss, FILENAME,run)
-  ),
-  file=stdout(),
-  col.names=FALSE,
-  row.names=FALSE
+if (miss > 0) warning("WARNING!!!",miss,"missed variable lookups occurred!")
+
+# Cache misses
+w <- system.cache(try(get('the',envir=e),silent=TRUE),gcFirst=TRUE,burnin=FALSE)
+cachemiss <- sum(unlist(w))
+
+results <- data.frame(
+  progname=progname,
+  constructtime=x,
+  searchtime=y,
+  memory=z[11] + z[12],
+  runtime=runsum,
+  hashsize=hashsize,
+  cachemiss=cachemiss,
+  datafile=datafile,
+  run=run
 )
+
+if (!file.exists(resfile)) {
+  write.table(results,file=resfile,row.names=FALSE)
+} else {
+  write.table(results,file=resfile,col.names=FALSE,row.names=FALSE,append=TRUE)
+}
